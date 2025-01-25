1e8dece15733aee8688836191c84f7875095ee47
stefanarsov <sarsov@uw.edu>
2025-01-25 16:35:54 -0600

    test

---
+++ ClassName.java
@@ -0,0 +1,20 @@
+public class ClassName {
+    // Fields (instance variables)
+    type variableName;
+
+    // Constructor
+    public ClassName(type parameter) {
+        this.variableName = parameter;
+    }
+
+    // Methods
+    public returnType methodName(type parameter) {
+        // method body
+        return returnValue;
+    }
+
+    // Main method (optional, if this is the main entry point of the program)
+    public static void main(String[] args) {
+        // Code to run when the class is executed
+    }
+}
