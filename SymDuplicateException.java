d00986ef73608b6b86fe4e0022d5260503c657f5
stefanarsov <sarsov@uw.edu>
2025-02-04 23:06:51 -0600

    updating symtab

--- SymDuplicateException.java
+++ SymDuplicateException.java
@@ -1,4 +1,8 @@
 public class SymDuplicateException extends Exception {
+    /**
+     * This method will throw an exception if the Sym is a duplicate
+     * @param message for the message contents of the exception
+     */
     public SymDuplicateException(String message) {
         super(message);
     }
