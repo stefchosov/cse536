1fcf36b1edbeddbe41d663f14e1f81246a5af13e
stefanarsov <sarsov@uw.edu>
2025-02-05 19:44:24 -0600

    deleted

--- SymDuplicateException.java
+++
@@ -1,9 +0,0 @@
-public class SymDuplicateException extends Exception {
-    /**
-     * This method will throw an exception if the Sym is a duplicate
-     * @param message for the message contents of the exception
-     */
-    public SymDuplicateException(String message) {
-        super(message);
-    }
-}
--- SymTabEmptyException.java
+++
@@ -1,9 +0,0 @@
-public class SymTabEmptyException extends Exception {
-    /**
-     * This method will throw an exception if the SymTab is empty
-     * @param message for the message of the exception
-     */
-    public SymTabEmptyException(String message) {
-        super(message);
-    }
-}
